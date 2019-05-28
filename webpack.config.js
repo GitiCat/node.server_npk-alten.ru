f12a2b76c094973fa2a17b27915c80cae4e8ae86
GitiCat <ivan_zhuravlev_1997@mail.ru>
2019-05-28 10:25:14 +0300

    base server settings

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: "/node_modules/",
+                loader: "babel-loader"
+            }
+        ]
+    },
+    devServer: {
+        port: 3000,
+        open: true,
+        proxy: {
+            "/api": "http://localhost:8080"
+        }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "src/template/default.html"
+        })
+    ]
+};
